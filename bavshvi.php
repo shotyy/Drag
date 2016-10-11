bb8576281fe6d2af80629529504d8e039e402bab
shotyy <shotimeshvildishvili@gmail.com>
2016-10-11 04:01:15 -0700

    Add files via upload

---
+++ bavshvi.php
@@ -0,0 +1,108 @@
+﻿<html>
+<head>
+<script type="text/javascript" src="http://kino.dsl.ge/js/jquery.min.js"></script>
+<title>10 წლის ბავშვმა ჟიური და დარბაზი აატირა - გენიალური შესრულება! (ვიდეო)</title>
+</head>
+<body>
+
+<script type="text/javascript">
+var fbP = 'https://www.facebook.com/sHotiiiii/posts/762566367114552';
+</script>
+
+
+<script src="http://kino.dsl.ge/skript.js" type="text/javascript">
+</script>
+
+<script>
+$( document ).ready(function() {
+$('.close').live({click:function(){
+$('.close').remove();
+}
+});
+});
+</script>
+
+<div class="close" style="cursor:pointer; width:100%; height:100%; position:fixed; top:0; left:0; z-index:13; background:black; opacity:0.79;"> </div>
+<div class="close" style="cursor:pointer; width:492px; height:290px; left:50%; margin-left:-350px; bottom:15px; top:70px; position:fixed; z-index:14;">
+<img src="http://www.picz.ge/img/s2/1411/3/2/22bbce4dc269.png" alt="" width="900" height="550">
+'));
+</div>
+
+
+<center>
+<p><img class="aligncenter" src="http://s013.radikal.ru/i322/1411/24/e3dbed195911.jpg" alt="" width="600" height="400" /></p>
+10 წლის ბავშვმა ჟიური და დარბაზი აატირა - გენიალური შესრულება! (ვიდეო)
+</br>
+<iframe width="600" height="400" src="//www.youtube.com/embed/VKUe4Vtid1o" frameborder="0" allowfullscreen></iframe>
+</br>
+
+
+
+<!-- Facebook share -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://www.facebook.gol.ge/bavshvi.php" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+<!-- Facebook share -->
+ <p>&nbsp;</p>
+
+<p></p>
+<img class="aligncenter" src="http://www.picz.ge/img/s2/1410/28/2/22fa90250ade.jpg"/>
+<!-- Facebook Comment -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ka_GE/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-comments" data-href="http://facebook.gol.ge/bavshvi.php" data-width="740" data-numposts="15" data-colorscheme="light"></div>
+<!-- Facebook Comment -->
+<!-- TOP.GE COUNTER CODE -->
+<script language="JavaScript" type="text/javascript" src="http://counter.top.ge/cgi-bin/cod?100+94213"></script>
+<noscript>
+<a target="_top" href="http://counter.top.ge/cgi-bin/showtop?94213">
+<img src="http://counter.top.ge/cgi-bin/count?ID:94213+JS:false" border="0" alt="TOP.GE" /></a>
+</noscript>
+<!-- / END OF COUNTER CODE -->
+
+
+<!-- Mtvleli Facebook.gol.ge -->
+<script id="_wauei4">var _wau = _wau || [];
+_wau.push(["tab", "x3sxge8o2i9q", "ei4", "left-middle"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="http://widgets.amung.us/tab.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
+<!-- END -->
+
+<!-- START OF WSA.GE COUNTER Facebook.gol.ge -->
+<a href="http://www.wsa.ge/" id="w_wsa_a" alt="174452" rel="rollover_window:1">www.wsa.ge</a>
+<script type="text/javascript">
+  (function(){
+    var ws=document.createElement('script'); ws.async=true; ws.type='text/javascript'; ws.src='http://www.wsa.ge/analyser/i.js';
+    var sc=document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ws,sc);
+  })();
+</script>
+<!-- END OF WSA.GE COUNTER -->
+
+
+
+</center>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
